247dba1b796e538fe305d932b4334a0400d6e48b
Aaron Ault <ault@purdue.edu>
2017-01-10 13:41:11 -0500

    Fixed edge case in sectionizer when there is activity and summary, but no positions.

--- sectionize.js
+++ sectionize.js
@@ -9,23 +9,17 @@ module.exports = input => {
   let state = 'start'; // start -> foundactivity -> foundpositions -> foundseg
   const result = _.reduce(lines, (acc,lineobj) => {
     const line = lineobj.line; // also have lineno
-    switch(state) {
-      case 'start': if (line.match(/Y O U R   A C T I V I T Y   T H I S   M O N T H/)) 
-                      state = 'foundactivity';
-                    if (line.match(/P O S I T I O N S   I N   Y O U R   A C C O U N T/))
-                      state = 'foundpositions';  // there wasn't any activity
-        break;
-      case 'foundactivity': if (line.match(/P O S I T I O N S   I N   Y O U R   A C C O U N T/))
-                              state = 'foundpositions';
-                            else
-                              acc.activity.push(lineobj);
-        break;
-      case 'foundpositions': if (line.match(/\*\*\* SEG USD \*\*\*/))
-                               state = 'foundseg';
-                             else
-                               acc.positions.push(lineobj);
-        break;
-      case 'foundseg': acc.summary.push(lineobj);
+    // If any of these lines are found at any time, change state:
+          if (line.match(/Y O U R   A C T I V I T Y   T H I S   M O N T H/))   state = 'foundactivity';
+    else  if (line.match(/P O S I T I O N S   I N   Y O U R   A C C O U N T/)) state = 'foundpositions';
+    else  if (line.match(/\*\*\* SEG USD \*\*\*/))                             state = 'foundseg';
+    else { // not a state-changing line, so push onto proper array
+      switch(state) {
+        case          'start':                              break;
+        case  'foundactivity': acc.activity.push(lineobj);  break;
+        case 'foundpositions': acc.positions.push(lineobj); break;
+        case       'foundseg': acc.summary.push(lineobj);   break;
+      }
     }
     return acc;
   },{activity: [], positions: [], summary: []});
